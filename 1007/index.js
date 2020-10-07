0034b6d2865759b14282ea31416962832ee92062
Z1N00-1015 <69869539+Z1N00-1015@users.noreply.github.com>
2020-10-07 21:27:43 +0900

    1007

--- 1007/index.js
+++ 1007/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+const books = [
+  {name: 'alice', author: 'jin', description: '엘리스가 집안에서 난동을 부리는 책입니다.'}];
+
+
 app.get('/', (req, res)=> {
-    res.send('Hello World!');
+  res.send(JSON.stringify(books));
 });
+
+
+app.get('/books/:id', (req, res) => {
+  const id = req.params.id;
+  const book = books.find((e) => e.id === id);
+  if (book === undefined) {
+    res.send({error: true, message: 'cannot find book'});
+  } else {
+    res.send(JSON.stringify(book));
+  }
+});
+
 
 app.listen(3000);
