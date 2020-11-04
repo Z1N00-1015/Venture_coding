c8cf1225b636364846a4a48f2552273096009c9d
Z1N00-1015 <69869539+Z1N00-1015@users.noreply.github.com>
2020-11-04 21:03:09 +0900

    mongo

--- 1104/test.js
+++ 1104/test.js
@@ -1,9 +1,10 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
+const mongoose = require('mongoose')
 
-const scrapping = fetch("http://cba.snu.ac.kr/ko/notice")
-    .then(function(html) {
-    const totalHtml = cheerio.load(html)
-    const titles = totalHtml.notice.title
-    console.log(titles)
-});
+mongoose.connect('mongodb+srv://zinoo:scot1015@cluster0.bzrdg.mongodb.net/Cluster0?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const Cat = mongoose.model('Cat', { name: String });
+const kitty = new Cat({ name: 'Zildjian' });
+
+kitty.save().then(() => console.log('meow'));
