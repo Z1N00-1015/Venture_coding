3ddfa542cc9fc9535e653d64b6024cbb799ad874
Z1N00-1015 <69869539+Z1N00-1015@users.noreply.github.com>
2020-11-05 16:16:04 +0900

    00

--- 1104/test.js
+++ 1104/test.js
@@ -1,10 +1,75 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+//깃헙에서 붙여넣기,,
+
+
+const express = require('express');
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://zinoo:scot1015@cluster0.bzrdg.mongodb.net/Cluster0?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const Cat = mongoose.model('Cat', { name: String });
-const kitty = new Cat({ name: 'Zildjian' });
+const Task = mongoose.model('Task',
+  {
+    title: String,
+    description: String,
+    dueDate: String,
+  },
+);
 
-kitty.save().then(() => console.log('meow'));
+
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+
+app.get('/tasks', async (req, res)=> {
+    const tasks = await Task.find()
+    res.send(tasks)
+})
+
+// tasks/1 => 0번째 task를 가져오는 함수
+app.get('/tasks/:id', async (req, res) => {
+    const id = await parseInt(req.params.id);
+    const task = await tasks[id - 1];
+  
+    if (!task) {
+      return(res.sendStatus(404));
+    }
+  
+    res.send(task);
+  });
+  
+  
+  // title, description, dueDate
+  app.post('/tasks', (req, res) => {
+    const { title, description, dueDate } = req.body;
+    const task = { title, description, dueDate };
+    tasks.push(task);
+  
+    res.send(task);
+  });
+  
+  // ?author=kim&page=1
+  app.put('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    const { title, description, dueDate} = req.body;
+    const task = {title, description, dueDate };
+    if (!task) {
+        return res.sendStatus(404);
+    }
+  
+    tasks[id - 1] = task;
+    task.title = title;
+    task.description = description;
+    task.dueDate = dueDate;
+    res.send(task);
+  });
+  
+  app.delete('/tasks/:id', (req, res) => {
+      const id = parseInt(req.params.id);
+      const task = tasks[id - 1];
+      if(!task) {
+          return res.sendStatus(404)};
+  
+      tasks.splice(id - 1, 1);
+      res.send(task)
+  });
+  
+  app.listen(3000);
