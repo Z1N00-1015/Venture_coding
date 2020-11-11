416931a28303b4cb873a93cadd963cedd203325a
Z1N00-1015 <69869539+Z1N00-1015@users.noreply.github.com>
2020-11-11 20:54:35 +0900

    asdf

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
-const func2 = (obj, arr) => {
-    const keys = Object.keys(obj);
-    keys.forEach(e=>arr.push(obj[e]));
-    return arr.filter(e => e%2 === 0);
+function func1(arr1, arr2) {
+    
 }
-
-
-
-console.log(func2({2: 5, 4: 6}, [5, 9, 10, 3]))
