3d4ca3d29a32475ab94e80cc3cb670e7e9ccfb2f
Z1N00-1015 <69869539+Z1N00-1015@users.noreply.github.com>
2020-11-11 18:38:19 +0900

    Update test.js

--- 1104/test.js
+++ 1104/test.js
@@ -15,61 +15,66 @@ const Task = mongoose.model('Task',
 );
 
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({extended:true}))
 
-app.get('/tasks', async (req, res)=> {
-    const tasks = await Task.find()
-    res.send(tasks)
-})
+app.use(express.urlencoded({extended: true}));
+
+app.get('/tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.send(tasks);
+});
+
 
 // tasks/1 => 0번째 task를 가져오는 함수
 app.get('/tasks/:id', async (req, res) => {
-    const id = await parseInt(req.params.id);
-    const task = await tasks[id - 1];
-  
-    if (!task) {
-      return(res.sendStatus(404));
-    }
-  
-    res.send(task);
-  });
-  
-  
-  // title, description, dueDate
-  app.post('/tasks', (req, res) => {
-    const { title, description, dueDate } = req.body;
-    const task = { title, description, dueDate };
-    tasks.push(task);
-  
-    res.send(task);
-  });
-  
-  // ?author=kim&page=1
-  app.put('/tasks/:id', (req, res) => {
-    const id = req.params.id;
-    const { title, description, dueDate} = req.body;
-    const task = {title, description, dueDate };
-    if (!task) {
-        return res.sendStatus(404);
-    }
-  
-    tasks[id - 1] = task;
-    task.title = title;
-    task.description = description;
-    task.dueDate = dueDate;
-    res.send(task);
-  });
-  
-  app.delete('/tasks/:id', (req, res) => {
-      const id = parseInt(req.params.id);
-      const task = tasks[id - 1];
-      if(!task) {
-          return res.sendStatus(404)};
-  
-      tasks.splice(id - 1, 1);
-      res.send(task)
-  });
-  
-  app.listen(3000);
+  const id = req.params.id;
+  const task = await Task.findById(id);
+  if (!task) {
+    return res.sendStatus(404);
+  }
+
+  res.send(task);
+});
+
+// title, description, dueDate
+app.post('/tasks', async (req, res) => {
+  const {title, description, dueDate} = req.body;
+  const task = new Task({title, description, dueDate});
+  await task.save();
+
+  res.send(task);
+});
+
+// ?author=kim&page=1
+app.put('/tasks/:id', async (req, res) => {
+  const id = req.params.id;
+  const task = await Task.findById(id);
+
+  if (!task) {
+    return res.sendStatus(404);
+  }
+
+  const {title, description, dueDate} = req.body;
+  task.title = title;
+  task.description = description;
+  task.dueDate = dueDate;
+
+  await task.save();
+
+  res.send(task);
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+  const id = req.params.id;
+  const task = await Task.findById(id);
+
+  if (!task) {
+    return res.sendStatus(404);
+  }
+  await task.delete();
+
+  res.sendStatus(200);
+});
+
+app.listen(3000);
